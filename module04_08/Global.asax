54eff9deb06f9bfe86179200814115db477f732e
auxo86 <auxo86@gmail.com>
2017-05-18 18:29:25 +0800

    module04_08

---
+++ module04_08/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="module04_08.MvcApplication" Language="C#" %>
---
+++ module04_08/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module04_08.Models
+{
+    class Comment
+    {
+        public int Id { get; set; }
+        public string Subject { get; set; }
+    }
+}
